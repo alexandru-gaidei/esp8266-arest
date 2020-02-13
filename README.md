375628f2897ca1eec1ab4622a0181420494df49a
Alexandru Gaidei <alexandru.gaidei@gmail.com>
2020-02-13 14:57:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+<h1>ESP8266 aREST DHT11/Led/Relay</h1>
+
+## Overview
+
+A simple example that implements a REST API for ESP8266 WiFi chip for handling DHT11 sensor and LED/Relay.  
+If you want to know more about aREST, go over to [http://arest.io/](http://arest.io/).
+
+## Setup
+
+1. Rename `env.h.example` to `env.h`
+2. Upload
+
+## Endpoints
+`http://wifi-ip`
+```
+{ 
+   "variables":{ 
+      "temperature":25.00,
+      "humidity":33.00,
+      "heatindex":24.42,
+      "relay":0
+   },
+   "id":"1",
+   "name":"Hall",
+   "hardware":"esp8266",
+   "connected":true
+}
+```
+
+`http://wifi-ip/temperature`
+```
+{ 
+   "temperature":25.00,
+   "id":"1",
+   "name":"Hall",
+   "hardware":"esp8266",
+   "connected":true
+}
+```
+
+`http://wifi-ip/relay-state?params=0|1`
+```
+{ 
+   "return_value":0|1,
+   "id":"1",
+   "name":"Hall",
+   "hardware":"esp8266",
+   "connected":true
+}
+```
---
+++ esp8266-arest.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>
+#include <aREST.h>
+#include <DHT.h>
+#include "env.h"
+
+
+int relayPin = 2;
+int sensorPin = 0;
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASS;
+
+String deviceId = "1";
+String deviceName = "Hall";
+String hostName = "hall";
+
+#define DHTPIN sensorPin
+#define DHTTYPE DHT11
+
+aREST rest = aREST();
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiServer server(80);
+
+float temperature, humidity, heatindex;
+int relayState;
+
+unsigned long sensorPreviousMillis = 0;
+const long sensorInterval = 2000; 
+
+int relayControl(String command);
+
+
+void setup(void)
+{
+  Serial.begin(115200);
+  dht.begin();
+
+  pinMode(relayPin, OUTPUT);
+
+  rest.variable("temperature",&temperature);
+  rest.variable("humidity",&humidity);
+  rest.variable("heatindex",&heatindex);
+  rest.variable("relay",&relayState);
+  rest.function("relay-state",relayControl);
+
+  rest.set_id(deviceId);
+  rest.set_name(deviceName);
+
+  Serial.println();
+
+  WiFi.hostname(hostName);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  server.begin();
+  Serial.println();
+  Serial.print(WiFi.localIP());
+  Serial.println();
+}
+
+
+void loop()
+{
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  while(!client.available()){
+    delay(1);
+  }
+
+  processSensorData();
+  rest.handle(client);
+  delay(1);
+}
+
+
+int relayControl(String command)
+{
+  int state = command.toInt();
+  digitalWrite(relayPin, state);
+  relayState = state;
+  return state;
+}
+
+
+void processSensorData() {
+  unsigned long sensorCurrentMillis = millis();
+ 
+  if(sensorCurrentMillis - sensorPreviousMillis >= sensorInterval) {
+    sensorPreviousMillis = sensorCurrentMillis;   
+ 
+    float ht  = dht.readHumidity();
+    float tt  = dht.readTemperature();
+    float hit = dht.computeHeatIndex(tt, ht, false);
+
+    if (isnan(ht) || isnan(tt) || isnan(hit)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      humidity = ht;
+      temperature = tt;
+      heatindex = hit;
+    }
+  }
+}
